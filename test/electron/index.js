8745800eac452ecc8bfd5fddf7bfc73b3249a064
simov <simeonvelichkov@gmail.com>
2016-09-01 11:40:38 +0300

    Move the ipcMain call to the electron-server

--- test/electron/index.js
+++ test/electron/index.js
@@ -9,16 +9,11 @@ var win = null
 
 app.on('ready', () => {
   win = new BrowserWindow({})
+  server(win)
   win.webContents.session.clearCache(() => {
     win.maximize()
     win.loadURL('file://' + __dirname + '/index.html')
     win.webContents.openDevTools()
     win.on('closed', () => (win = null))
   })
-})
-
-ipcMain.on('message', (event, req) => {
-  if (req.message === 'connect') {
-    server(win)
-  }
 })

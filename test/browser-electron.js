54e41d933583bfedcb73a9dbdaf7c194303c3255
simov <simeonvelichkov@gmail.com>
2016-09-01 12:41:12 +0300

    Add browser-server test

---
+++ test/browser-electron.js
@@ -0,0 +1,48 @@
+
+/*
+  1. admin: start Electron process (ws)
+  2. user: connect a web socket to it (browser)
+  3. electron: load the webview and wait for it to load
+  4. electron: send `connect` message back to the electron-server
+  5. electron: write to stdout to indicate that the electron-server is ready
+*/
+
+var config = require('../config')[process.env.NODE_ENV || 'development']
+var t = require('assert')
+var path = require('path')
+var cp = require('child_process')
+var engine = require('engine.io-client')
+
+
+describe('browser-electron', () => {
+  var server, client
+
+  before((done) => {
+    server = cp.spawn('electron', [path.join(process.cwd(), 'test/electron/')])
+    server.stdout.on('data', (e) => {
+      var output = e.toString().trim()
+      console.log(output)
+
+      if (/electron-server/.test(output)) {
+        client = engine('ws://localhost:' + config.electron)
+        client.on('open', () => done())
+      }
+    })
+
+    server.stderr.on('data', (e) => console.log(e.toString().trim()))
+  })
+
+  it('send message', (done) => {
+    client.send(JSON.stringify({message: 'test'}))
+
+    client.once('message', (req) => {
+      t.deepEqual(JSON.parse(req), {message: 'test', body: 'webview'})
+      done()
+    })
+  })
+
+  after((done) => {
+    server.kill('SIGHUP')
+    done()
+  })
+})
